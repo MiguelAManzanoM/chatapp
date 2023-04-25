99b847ad2820766d37039c70b74d8d3ba6d94fdf
MiguelAManzanoM <105388118+MiguelAManzanoM@users.noreply.github.com>
2023-04-25 03:01:42 -0600

    Primer commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    APP_ID: "1:998645040899:web:6cf8b81a704ec65fa10b36",
+    MESSAGING_SENDER_ID: "998645040899",
+    STORAGE_BUCKET: "proyectofrontend-15e4a.appspot.com",
+    PROJECT_ID: "proyectofrontend-15e4a",
+    AUTH_DOMAIN: "proyectofrontend-15e4a.firebaseapp.com",
+    API_KEY: "AIzaSyAC5EUBZSaVakrnjYv-PARnEkMx2XayXFY",
+  },
+};
